c56910e5e62a9a27166c7d07532555bf388b2780
WilliGomez <willgom.95@gmail.com>
2023-07-11 23:41:35 -0500

    Primer commit

---
+++ script1.js
@@ -0,0 +1,12 @@
+/**let numeros = [];
+
+for (let i = 0; i < 10; i++) {
+  
+ 
+const numeroAleatorio = Math.floor(Math.random() * 100); 
+  numeros.
+ 
+push(numeroAleatorio); y
+}
+
+console.log(numeros);*/
---
+++ script2.js
@@ -0,0 +1,10 @@
+/**const num = [];
+
+const count = num.push('cows');
+console.log(count);
+
+console.log(num);
+
+
+num.push('1', '2', '3', '4', '5');
+console.log(num);*/
